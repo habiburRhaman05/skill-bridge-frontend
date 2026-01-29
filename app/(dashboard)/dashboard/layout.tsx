2939ed7632976fba19581379636699121e16c9c6
habiburRhaman05 <devhabib2005@gmail.com>
2026-01-29 07:02:40 +0600

    Initial commit from Create Next App

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StudentDashboardLayout = ({children}:{
+    children:React.ReactNode
+}) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default StudentDashboardLayout
